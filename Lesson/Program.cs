474b78dd5c40bb8d5a6f7e21e73d0eb14ef83185
RiiFU <RiiFU@RIIFUS->
2022-01-25 22:05:13 +0700

    Добавьте файлы проекта.

---
+++ Lesson/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите имя пользователя: ");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Привет, {name}, сегодня, {DateTime.Now.ToString()}!");
+            Console.ReadKey();
+     
+        }
+    }
+}
