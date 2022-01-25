20c16877ee38a173a71e14a07abd019cb630ef01
RiiFU <di.22rus@yandex.ru>
2022-01-25 22:22:27 +0700

    1111

--- Lesson/Program.cs
+++ Lesson/Program.cs
@@ -10,9 +10,9 @@ namespace Lesson
     {
         static void Main(string[] args)
         {
-            Console.Write("Введите имя пользователя: ");
-            var name = Console.ReadLine();
-            Console.WriteLine($"Привет, {name}, сегодня, {DateTime.Now.ToString()}!");
+            Console.Write("Введите имя пользователя: "); //Выводим консоль с сообщением
+            var name = Console.ReadLine(); //Записываем имя пользователя из консоли в переменную
+            Console.WriteLine($"Привет, {name}, сегодня, {DateTime.Now.ToString()}!"); //Выводим текст в консоль
             Console.ReadKey();
      
         }
